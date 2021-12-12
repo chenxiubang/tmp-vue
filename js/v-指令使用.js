be483d367c47d89c78e7c5028c0430467d22b3c4
omen <1094978730@qq.com>
2021-12-12 21:07:44 +0800

    no msg

--- js/v-指令使用.js
+++ js/v-指令使用.js
@@ -1,10 +1,15 @@
-const app = new Vue({
-  el:document.getElementById("container"),
-  data: {
-    hello: "hello",
-    htmlText: "<a href='https://www.baidu.com/'>www.baidu.com</a>"
-  },
-  methods: {
 
-  }
-})
+//延迟一秒钟后渲染
+  setTimeout(() => {
+    console.log("延迟1秒钟");
+    const app = new Vue({
+      el:document.getElementById("container"),
+      data: {
+        hello: "hello",
+        htmlText: "<a href='https://www.baidu.com/'>www.baidu.com</a>"
+      },
+      methods: {
+    
+      }
+    })
+  }, 1000);
