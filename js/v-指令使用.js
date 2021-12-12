093a8696ff145bb232498d26a98e48b2b216aa2b
omen <1094978730@qq.com>
2021-12-12 20:46:57 +0800

    no msg

--- js/v-指令使用.js
+++ js/v-指令使用.js
@@ -1,7 +1,8 @@
 const app = new Vue({
   el:document.getElementById("container"),
   data: {
-    hello: "hello"
+    hello: "hello",
+    htmlText: "<a href='https://www.baidu.com/'>www.baidu.com</a>"
   },
   methods: {
 
